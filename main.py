20ea6549ecf3862f778a3f4f6cfc7e597dbf5d8e
Noah Abe <noahabe64@gmail.com>
2021-01-09 08:16:41 +0300

    added a non recusive fibonacci function

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ def fib(n):
 		return n
 	return fib(n-1) + fib(n-2)
 
+def fib_not_recusive(n):
+	x = 0
+	y = 1
+	if n == 0:
+		return x
+	elif n == 1:
+		return y
+	while n-2 >= 0:
+		x,y = y,x+y
+		n -= 1 
+	return y
+
 if __name__ == '__main__':
 	f()
 
