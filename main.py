f99b1bf6ead4d1a661036b5df209a24dbc82260f
Noah Abe <noahabe64@gmail.com>
2021-01-09 08:20:28 +0300

    fixed a typo recusive -> recursive

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def fib(n):
 		return n
 	return fib(n-1) + fib(n-2)
 
-def fib_not_recusive(n):
+def fib_not_recursive(n):
 	x = 0
 	y = 1
 	if n == 0:
@@ -21,6 +21,6 @@ def fib_not_recusive(n):
 
 if __name__ == '__main__':
 	fib_generated_by_recurse = [fib(i) for i in range(0,20)]
-	fib_generated_by_non_recurse = [fib_not_recusive(i) for i in range(0,20)]
+	fib_generated_by_non_recurse = [fib_not_recursive(i) for i in range(0,20)]
 	print(fib_generated_by_recurse)
 	print(fib_generated_by_non_recurse)
